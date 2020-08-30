96975552d5f4d1f2d6ef244066c771ed00afe33d
humayunadilshahzad <humayun_adil@hotmail.com>
2020-08-30 18:53:36 +0200

    images folder with images, project and home components added

---
+++ src/components/project/project.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './project.css';
+import GitLinkLogo from'../../images/GitHub-Mark-64px.png';
+import WebLinkLogo from'../../images/goto.jpg';
+function Project(props) {
+  return (
+    <div className='project'>
+        <h1>{props.title}</h1>
+        {/* <h3>{props.pic}</h3> */}
+        <img className='appimg' src={props.pic} alt='Pic'></img>
+        <div className='links'>
+            <a href={props.gitResourceLink} target="_blank" title='view code on git'><img className='linkimg' src={GitLinkLogo} alt='Pic'/></a>
+            <a href={props.liveLink} target="_blank" title='Goto webSite'>< img className='linkimg' src={WebLinkLogo} alt='Pic' /></a>
+            {/* <button>See Live</button>
+            <button>GitHub Code</button> */}
+        </div>
+    </div>
+  );
+}
+ 
+
+export default Project;
---
+++ src/pages/home/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './home.css'
+
+function HomePage() {
+    return(
+        <h1>This is home page</h1>
+    );
+}
+
+export default HomePage;
