223dbb46cd6edef0f68c0890fa6e1c5ead70c436
humayunadilshahzad <humayun_adil@hotmail.com>
2020-08-30 18:45:59 +0200

    roots added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/top_menu/topMenu'
 import About from './pages/about/about'
+import Home from './pages/home/home'
 import Projects from './pages/projects/projects'
 import Qualification from './pages/qualification/qualification'
 import Leasure from './pages/leasure/leasure'
@@ -18,6 +19,7 @@ function App() {
       <header>
         <NavBar />
           <Switch>
+            <Route path='/' exact component={Home}/>
             <Route path='/about' component={About}/>
             <Route path='/projects' component={Projects} />
             <Route path='/qualification' component={Qualification} />
