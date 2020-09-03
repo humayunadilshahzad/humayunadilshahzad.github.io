b2bc318b0711b26369dc3971653b21e146a582c7
humayunadilshahzad <humayun_adil@hotmail.com>
2020-09-04 00:26:52 +0200

    project page updated and top menu color and style updated

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -4,10 +4,8 @@ import Project from '../../components/project/project';
 import SeasoneyAppPic from '../../images/seasonyApp1.png';
 import weatherAppPic from '../../images/weatherApp.png';
 import giffyAppPic from '../../images/giffyApp.png';
+import circlesAppPic from '../../images/circles.png';
 
-// weather app URL  https://htmlpreview.github.io/?https://github.com/humayunadilshahzad/hyf-homework/blob/master/javascript/javascript3/week1/weather%20app/index.html
-// giffy app URL https://htmlpreview.github.io/?https://github.com/humayunadilshahzad/hyf-homework/blob/master/javascript/javascript3/week1/giphy%20app/index.html
-//http://htmlpreview.github.io/?https://github.com/humayunadilshahzad/hyf-homework/blob/master/javascript/javascript3/week1/giphy%20app/index.html
 
 // this "proj" const array of objects will later be replaced by API call  
 const proj =[ 
@@ -21,10 +19,10 @@ const proj =[
     githubSourceLink:'https://github.com/humayunadilshahzad/meals-sharing',
     webLink:'#'},
     
-    {title:'Seasoney',
-    pic:SeasoneyAppPic,
-    githubSourceLink:'https://github.com/HackYourFuture-CPH/FP-class11',
-    webLink:'https://fp-class11.herokuapp.com/dashboard'},
+    {title:'Color full Circles',
+    pic:circlesAppPic,
+    githubSourceLink:'https://github.com/humayunadilshahzad/hyf-homework/tree/master/javascript/javascript3/week3/cirles',
+    webLink:'http://htmlpreview.github.io/?https://github.com/humayunadilshahzad/hyf-homework/blob/master/javascript/javascript3/week3/cirles/index.html'},
     
     {title:'Weather App',
     pic:weatherAppPic,
@@ -39,7 +37,8 @@ const proj =[
     {title:'Weather App',
     pic:weatherAppPic,
     githubSourceLink:'https://github.com/humayunadilshahzad/hyf-homework/tree/master/javascript/javascript3/week1/weather%20app',
-    webLink:'https://htmlpreview.github.io/?https://github.com/humayunadilshahzad/hyf-homework/blob/master/javascript/javascript3/week1/weather%20app/index.html'}
+    webLink:'https://htmlpreview.github.io/?https://github.com/humayunadilshahzad/hyf-homework/blob/master/javascript/javascript3/week1/weather%20app/index.html'},
+    
   ]
 
 function Projects() {
