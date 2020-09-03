644f8c92e3bcb97961d7b1deae2634b5082dbca3
humayunadilshahzad <humayun_adil@hotmail.com>
2020-09-04 00:16:44 +0200

    footer added

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+function Footer(){
+    return(
+        <div className='foot'>
+            <a href='#'>linkedIn</a>
+            <a href='#'>Github</a>
+            
+        </div>
+    )
+}
+export default Footer;
