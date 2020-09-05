9861a8b29b6af441de40387ca3ad7d2c3a270e2c
humayunadilshahzad <humayun_adil@hotmail.com>
2020-09-05 22:45:47 +0200

    project component and css updated

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -5,9 +5,10 @@ import WebLinkLogo from'../../images/goto.jpg';
 function Project(props) {
   return (
     <div className='project'>
-        <h1>{props.title}</h1>
+        <h2>{props.title}</h2>
         {/* <h3>{props.pic}</h3> */}
         <img className='appimg' src={props.pic} alt='Pic'></img>
+        <p>{props.about}</p>
         <div className='links'>
             <a href={props.gitResourceLink} target="_blank" rel="noopener noreferrer" title='view code on git'><img className='linkimg' src={GitLinkLogo} alt='Pic'/></a>
             <a href={props.liveLink} target="_blank" rel="noopener noreferrer" title='Goto webSite'>< img className='linkimg' src={WebLinkLogo} alt='Pic' /></a>
