3cca46a958cf0db1d6fe32a0362e9700d4cc0873
humayunadilshahzad <humayun_adil@hotmail.com>
2020-09-05 22:44:23 +0200

    about page and css added

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './about.css';
+import myPic from '../../images/mealSharing.jpg'
 
 function About() {
   return (
-    <div>
-        <h1>This is About Page</h1>
+    <div className='abc'>
+      <div className='textabout'>
+        <h3>About</h3>
+        <p className='alignleft'>This is about me.This is about me.This is about me.
+              This is about me.This is about me.This is about me.
+          This is about me.This is about me.This is about me.This is about me.
+          This is about me.This is about me.This is about me.This is about me.
+          This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.
+          This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.
+          This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.
+          This is about me.This is about me.This is about me.This is about me.
+          This is about me.</p>
+              <p className='alignleft'>This is about me.This is about me.This is about me.This is about me.
+          This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.This is about me.
+          This is about me.This is about me.This is about me.This is about me.
+          </p>
+
+      </div>
+      <img src={myPic} alt='Pic'></img>
     </div>
   );
 }
