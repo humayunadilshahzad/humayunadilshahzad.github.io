d53a8804abe871404c4c82e2b5381915e25a40fe
humayunadilshahzad <humayun_adil@hotmail.com>
2020-09-08 21:14:10 +0200

    project component updated

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -6,15 +6,13 @@ function Project(props) {
   return (
     <div className='project'>
         <h2>{props.title}</h2>
-        {/* <h3>{props.pic}</h3> */}
-        <img className='appimg' src={props.pic} alt='Pic'></img>
+
+            <img className='appimg' src={props.pic} alt='Pic'></img>
         <p>{props.about}</p>
-        <div className='links'>
+    <div className='links'>
             <a href={props.gitResourceLink} target="_blank" rel="noopener noreferrer" title='view code on git'><img className='linkimg' src={GitLinkLogo} alt='Pic'/></a>
             <a href={props.liveLink} target="_blank" rel="noopener noreferrer" title='Goto webSite'>< img className='linkimg' src={WebLinkLogo} alt='Pic' /></a>
-            {/* <button>See Live</button>
-            <button>GitHub Code</button> */}
-        </div>
+                </div>
     </div>
   );
 }
