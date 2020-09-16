038547b22f7a99572dade4d058a64941032b2587
humayunadilshahzad <humayun_adil@hotmail.com>
2020-09-16 22:02:20 +0200

    home component is replaced with about component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/top_menu/topMenu'
 import About from './pages/about/about'
-import Home from './pages/home/home'
+// import Home from './pages/home/home'
 import Projects from './pages/projects/projects'
 import Qualification from './pages/qualification/qualification.page'
 import Leasure from './pages/leasure/leasure'
@@ -19,7 +19,7 @@ function App() {
       <header>
         <NavBar />
           <Switch>
-            <Route path='/' exact component={Home}/>
+            <Route path='/' exact component={About}/>
             <Route path='/about' component={About}/>
             <Route path='/projects' component={Projects} />
             <Route path='/qualification' component={Qualification} />
