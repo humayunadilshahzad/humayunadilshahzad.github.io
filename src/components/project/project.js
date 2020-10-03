e52affa02c3c7880a9fe3956e40e6309a418ba50
humayunadilshahzad <humayun_adil@hotmail.com>
2020-10-03 11:56:30 +0200

    card flipping added

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './project.css';
-import GitLinkLogo from'../../images/GitHub-Mark-64px.png';
-import WebLinkLogo from'../../images/goto.jpg';
+// import GitLinkLogo from'../../images/GitHub-Mark-64px.png';
+// import WebLinkLogo from'../../images/goto.jpg';
 function Project(props) {
   return (
     <div className='project'>
         <h2>{props.title}</h2>
-
-            <img className='appimg' src={props.pic} alt='Pic'></img>
+        <img className='appimg' src={props.pic} alt='Pic'></img>
         <p>{props.about}</p>
-    <div className='links'>
+        
+        {/* <div className='links'>
             <a href={props.gitResourceLink} target="_blank" rel="noopener noreferrer" title='view code on git'><img className='linkimg' src={GitLinkLogo} alt='Pic'/></a>
             <a href={props.liveLink} target="_blank" rel="noopener noreferrer" title='Goto webSite'>< img className='linkimg' src={WebLinkLogo} alt='Pic' /></a>
-                </div>
+                </div> */}
     </div>
   );
 }
