bd50a11606747fff6f10e48045cf1edc0cf6f847
humayunadilshahzad <humayun_adil@hotmail.com>
2021-03-06 01:12:07 +0100

    footer links added

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,9 +3,9 @@ import './footer.css'
 function Footer(){
     return(
         <div className='foot'>
-            <a href='#'>linkedIn</a>
-            <a href='#'>Github</a>
-            <a href='#'>facebook</a>
+            <a href='https://www.linkedin.com/in/humayun-adil-21771071/' target="blank" >linkedIn</a>
+            <a href='https://github.com/humayunadilshahzad' target="blank">Github</a>
+            <a href='https://www.facebook.com/humayun.adil' target="blank">facebook</a>
             
         </div>
     )
