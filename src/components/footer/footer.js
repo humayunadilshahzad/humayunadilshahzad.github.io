a9f6bc91242ecc8339f3625bfa47df94df7e3ef3
humayunadilshahzad <humayun_adil@hotmail.com>
2021-03-07 02:29:24 +0100

    media icons added in footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {FaGithub, FaFacebook, FaLinkedin} from "react-icons/fa"
 import './footer.css'
+//import { icons } from 'react-icons';
 function Footer(){
     return(
         <div className='foot'>
-            <a href='https://www.linkedin.com/in/humayun-adil-21771071/' target="blank" >linkedIn</a>
-            <a href='https://github.com/humayunadilshahzad' target="blank">Github</a>
-            <a href='https://www.facebook.com/humayun.adil' target="blank">facebook</a>
+            <a href='https://www.linkedin.com/in/humayun-adil-21771071/' target="blank" ><FaLinkedin/>linkedIn</a>
+            <a href='https://github.com/humayunadilshahzad' target="blank"><FaGithub/> Github</a>
+            <a href='https://www.facebook.com/humayun.adil' target="blank"><FaFacebook/>facebook</a>
             
         </div>
     )
