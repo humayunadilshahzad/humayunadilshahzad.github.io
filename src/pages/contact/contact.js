e8c16c081a0dd54a318922daa7f8cbdfecb23e23
humayunadilshahzad <humayun_adil@hotmail.com>
2021-03-20 02:47:34 +0100

    contact form and styles added

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './contact.css';
+import {FaMailBulk} from "react-icons/fa"
+
 //import NavBar from './components/top_menu/topMenu'
 
 function Contact() {
   return (
-    <div>
-        <h1>This is Contact Page</h1>
+    <div className="contactform">
+        <h1>Contact Me</h1>
+        <img src="" alt="letter Pic"></img>
+        <FaMailBulk className='img'/>
+        <input></input>
+        <input></input>
+        <input></input>
+        <button>Submit</button>
+        
         <h3>Humayun_adil@hotmail.com</h3>
-    </div>
+    </div>                                              
   );
 }
 
