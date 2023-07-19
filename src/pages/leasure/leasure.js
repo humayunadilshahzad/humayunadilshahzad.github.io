5295eeae1810d9ee016505fa0a26aee0e54eb25c
humayunadilshahzad <humayun_adil@hotmail.com>
2023-07-19 21:58:37 +0200

    Leasure activites added and projects redefined

--- src/pages/leasure/leasure.js
+++ src/pages/leasure/leasure.js
@@ -3,11 +3,15 @@ import './leasure.css';
 
 function Leasure() {
   return (
-    <div>
+    <div className='Leasure-formate'>
         <h1>Activities done in me free time</h1>
         <ul>
           <li>Playing with Kids</li>
           <li>Watching you tube</li>
+          <li>Coding with React js</li>
+          <li>Cleaning home</li>
+          <li>Cooking</li>
+          <li>Workout</li>
         </ul>
     </div>
   );
